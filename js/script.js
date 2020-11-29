08fa3a1f641a00aa1768b85b94e1b93dc8dcdd51
Dimas W <daimessdn@gmail.com>
2020-11-29 13:40:25 +0700

    set new window size

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function dummyExec_(command) {
 		
 		terminal_msg.innerHTML += "Opening <em>" + command + "</em>, plase wait...<br />";
 
-		window.open(window.location.origin + "/" + command, "_blank", height=500, width=400);
+		window.open(window.location.origin + "/" + command, "_blank", "height=500,width=400");
 
 		terminal_msg.innerHTML += `${command} opened.`;
 
