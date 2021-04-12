dc2c41d81f8741afc7409d5cb212cf7a81809320
Dimas Wihandono <daimessdn@gmail.com>
2021-04-12 21:19:58 +0700

    added physical filesystem prototype

---
+++ js/fs.js
@@ -0,0 +1,46 @@
+const filesystem = [
+  {
+    name: "home",
+    type: "directory",
+    content: [
+      {
+        name: "journal",
+        type: "app",
+      },
+      {
+        name: "simpth",
+        type: "app",
+      },
+      {
+        name: "Downloads",
+        type: "directory",
+        content: [],
+      },
+      {
+        name: "Documents",
+        type: "directory",
+        content: [],
+      },
+      {
+        name: "Desktop",
+        type: "directory",
+        content: [],
+      },
+      {
+        name: "Videos",
+        type: "directory",
+        content: [],
+      },
+      {
+        name: "Pictures",
+        type: "directory",
+        content: [],
+      },
+      {
+        name: "Music",
+        type: "directory",
+        content: [],
+      },
+    ],
+  },
+];
